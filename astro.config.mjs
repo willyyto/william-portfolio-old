bc6391d33baf24aafc7ed0a292182c816390a519
willyyto <williamto222@gmail.com>
2022-12-22 22:44:15 +1100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.williamto.com",
+  site: "'https://willyyto.github.io'",
   base: '/william-portfolio',
   integrations: [
     tailwind(),
